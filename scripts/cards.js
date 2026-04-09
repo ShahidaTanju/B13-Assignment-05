aa12272b3fda9c737ea133bafe5f10d73da61691
ShahidaTanju <shahidasharmin450@gmail.com>
2026-04-09 17:22:38 +0600

    card-filter

--- scripts/cards.js
+++ scripts/cards.js
@@ -26,38 +26,38 @@ function toggleStyle(id) {
 
     const selected = document.getElementById(id);
     currentStatus = id
+    console.log(currentStatus);
 
     selected.classList.remove("bg-gray-200");
     selected.classList.add("bg-blue-600", "text-white");
 
-    if (id === "open-btn") {
-        allCardSection.classList.add("hidden");
-        filteredSection.classList.remove("hidden");
+    if (id === "all-btn") {
+        loadCards();
     }
-    else if (id === "all-btn") {
-        allCardSection.classList.remove("hidden");
-        filteredSection.classList.add("hidden");
+
+    else if (id === "open-btn") {
+        fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+            .then(res => res.json())
+            .then(data => {
+                const openData = data.data.filter(card => card.status === "open");
+                displayCards(openData);
+            });
     }
+
     else if (id === "closed-btn") {
-        allCardSection.classList.add("hidden");
-        filteredSection.classList.remove("hidden");
+        fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+            .then(res => res.json())
+            .then(data => {
+                const closedData = data.data.filter(card => card.status === "closed");
+                displayCards(closedData);
+            });
     }
 }
 
+
+
 // card count :
 
-const allCount = document.getElementById("all-count");
-const openCount = document.getElementById("open-count");
-const closedCount = document.getElementById("closed-count");
-
-
-// function createCard(item) {
-//     return `
-//     <div class="p-3 rounded shawdow" ${item.status === "open" ? "bg-green-100" : "bg-purple-100"}>
-//            <span></span>
-//     </div>
-//     `
-// }
 
 // fetching data :
 
@@ -74,20 +74,14 @@ const displayCards = (cards) => {
     const container = document.getElementById("cardContainer");
     container.innerHTML = "";
 
-    let openCount = 0;
-    let closedCount = 0;
-
     for (let card of cards) {
 
-        // card calculate:
-
-        if (card.status === "open") openCount++;
-        if (card.status === "closed") closedCount++;
+        const borderColor = card.status === "open" ? "border-green-500" : card.status === "closed" ? "border-purple-500" : "border-gray-300";
 
         const div = document.createElement("div");
 
-        div.innerHTML = `<div class="w-65 h-60 bg-white p-4 rounded-lg shadow border-t-2 ${card.status === "open" ? "bg-green-400" : "bg-purple-400"}">
-        <span class="px-2 py-1 bg-gray-100 rounded text-sm
+        div.innerHTML = `<div class="w-65 h-60 bg-white p-4 rounded-lg shadow border-t-4 ${borderColor}">
+        <span class="px-2 py-1 rounded text-sm flex justify-end items-end
         ">${card.priority}</span>
         <h2 class="font-semibold text-sm mb-2">${card.title}</h2>
         
